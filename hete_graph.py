102642ec9cd438f6d3789a0360845e1131974fff
Zhejian Peng <ZHEJIANPENG@YAHOO.COM>
2021-03-16 13:43:30 -0500

    Init

---
+++ hete_graph.py
@@ -0,0 +1,261 @@
+"""
+Create a Heterogeneous graph for edges in different years.
+(node, year, node), where year can be used a feature of embedding
+"""
+
+import argparse
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+from torch_sparse import SparseTensor
+import torch_geometric.nn as pyg_nn
+import torch_geometric.transforms as T
+from torch_geometric.nn import GCNConv, SAGEConv
+from torch_geometric.data import Data, Dataset, InMemoryDataset, DataLoader
+from torch_scatter import scatter
+
+from ogb.linkproppred import PygLinkPropPredDataset, Evaluator
+from deepsnap.hetero_gnn import forward_op
+from deepsnap.hetero_graph import HeteroGraph
+from torch_sparse import SparseTensor, matmul
+from sklearn.metrics import f1_score
+
+from torch_geometric.utils import to_networkx
+
+from logger import Logger
+
+from hete_models import *
+
+# Please do not change the following parameters
+args = {
+    'device': torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+    'hidden_size': 64,
+    'weight_decay': 1e-5,
+    'lr': 0.003,
+    'attn_size': 32,
+    'run': 2,
+    'epochs': 10,
+    'batch_size': 64,
+    'eval_steps': 1,
+    'log_steps': 1
+}
+
+
+
+def train(model, predictor, hetero_graph, split_edge, optimizer, batch_size):
+    model.train()
+    predictor.train()
+
+    pos_train_edge = split_edge['train']['edge'].to(hetero_graph.node_label['author'].device
+)
+
+    total_loss = total_examples = 0
+    for perm in DataLoader(range(10000), batch_size,
+                           shuffle=True):
+    # for perm in DataLoader(range(pos_train_edge.size(0)), batch_size,
+    #                        shuffle=True):
+        optimizer.zero_grad()
+
+        h = model(hetero_graph.node_feature, hetero_graph.edge_index) # encode every edges with GNN model
+
+        edge = pos_train_edge[perm].t() # (2, B)
+
+        pos_out = predictor(h['author'][edge[0]], h['author'][edge[1]])
+        pos_loss = -torch.log(pos_out + 1e-15).mean()
+
+        # Just do some trivial random sampling.
+        edge = torch.randint(0, hetero_graph.num_nodes()['author'], edge.size(), dtype=torch.long,
+                             device=h['author'].device)
+        neg_out = predictor(h['author'][edge[0]], h['author'][edge[1]])
+        neg_loss = -torch.log(1 - neg_out + 1e-15).mean()
+
+        loss = pos_loss + neg_loss
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        torch.nn.utils.clip_grad_norm_(predictor.parameters(), 1.0)
+
+        optimizer.step()
+
+        num_examples = pos_out.size(0)
+        total_loss += loss.item() * num_examples
+        total_examples += num_examples
+
+    return total_loss / total_examples
+
+
+
+@torch.no_grad()
+def test(model, predictor, data, split_edge, evaluator, batch_size):
+    model.eval()
+    predictor.eval()
+
+    h = model(data.x, data.adj_t)
+
+    pos_train_edge = split_edge['train']['edge'].to(h.device)
+    pos_valid_edge = split_edge['valid']['edge'].to(h.device)
+    neg_valid_edge = split_edge['valid']['edge_neg'].to(h.device)
+    pos_test_edge = split_edge['test']['edge'].to(h.device)
+    neg_test_edge = split_edge['test']['edge_neg'].to(h.device)
+
+    pos_train_preds = []
+    for perm in DataLoader(range(pos_train_edge.size(0)), batch_size):
+        edge = pos_train_edge[perm].t()
+        pos_train_preds += [predictor(h[edge[0]], h[edge[1]]).squeeze().cpu()]
+    pos_train_pred = torch.cat(pos_train_preds, dim=0)
+
+    pos_valid_preds = []
+    for perm in DataLoader(range(pos_valid_edge.size(0)), batch_size):
+        edge = pos_valid_edge[perm].t()
+        pos_valid_preds += [predictor(h[edge[0]], h[edge[1]]).squeeze().cpu()]
+    pos_valid_pred = torch.cat(pos_valid_preds, dim=0)
+
+    neg_valid_preds = []
+    for perm in DataLoader(range(neg_valid_edge.size(0)), batch_size):
+        edge = neg_valid_edge[perm].t()
+        neg_valid_preds += [predictor(h[edge[0]], h[edge[1]]).squeeze().cpu()]
+    neg_valid_pred = torch.cat(neg_valid_preds, dim=0)
+
+    h = model(data.x, data.full_adj_t)
+
+    pos_test_preds = []
+    for perm in DataLoader(range(pos_test_edge.size(0)), batch_size):
+        edge = pos_test_edge[perm].t()
+        pos_test_preds += [predictor(h[edge[0]], h[edge[1]]).squeeze().cpu()]
+    pos_test_pred = torch.cat(pos_test_preds, dim=0)
+
+    neg_test_preds = []
+    for perm in DataLoader(range(neg_test_edge.size(0)), batch_size):
+        edge = neg_test_edge[perm].t()
+        neg_test_preds += [predictor(h[edge[0]], h[edge[1]]).squeeze().cpu()]
+    neg_test_pred = torch.cat(neg_test_preds, dim=0)
+
+    results = {}
+    for K in [10, 50, 100]:
+        evaluator.K = K
+        train_hits = evaluator.eval({
+            'y_pred_pos': pos_train_pred,
+            'y_pred_neg': neg_valid_pred,
+        })[f'hits@{K}']
+        valid_hits = evaluator.eval({
+            'y_pred_pos': pos_valid_pred,
+            'y_pred_neg': neg_valid_pred,
+        })[f'hits@{K}']
+        test_hits = evaluator.eval({
+            'y_pred_pos': pos_test_pred,
+            'y_pred_neg': neg_test_pred,
+        })[f'hits@{K}']
+
+        results[f'Hits@{K}'] = (train_hits, valid_hits, test_hits)
+
+    return results
+
+# Transfer data to heterogenous graph
+dataset = PygLinkPropPredDataset(name='ogbl-collab')
+data = dataset[0]
+
+# Specify all the message types
+years = torch.unique(data.edge_year).numpy().astype(int)
+msg_types = []
+for y in years:
+    msg_type = ("author", str(y), "author")
+    msg_types.append(msg_type)
+
+# Dictionary of edge indices
+edge_index = {}
+for mt in msg_types:
+    y = int(mt[1])
+    yr_idx = (data.edge_year==y).T[0]
+    edge_idx = data.edge_index.T[yr_idx]
+    edge_idx = torch.cat([edge_idx[edge_idx[:,0] < edge_idx[:,1]], \
+                edge_idx[edge_idx[:,0] > edge_idx[:,1]]]).T
+    edge_index[mt] = edge_idx
+
+# Dictionary of node features
+node_feature = {}
+node_feature["author"] = data.x
+
+# Dictionary of edge indices, we have one type of node "author"
+node_label = {}
+node_label['author'] = torch.zeros(data.num_nodes, dtype=int)
+
+# Construct a deepsnap tensor backend HeteroGraph
+hetero_graph = HeteroGraph(
+    node_feature=node_feature,
+    node_label=node_label,
+    edge_index=edge_index,
+    directed=False
+)
+print(f"Collab heterogeneous graph: {hetero_graph.num_nodes()} nodes, {hetero_graph.num_edges()} edges")
+
+# Node feature and node label to device
+for key in hetero_graph.node_feature:
+    hetero_graph.node_feature[key] = hetero_graph.node_feature[key].to(args['device'])
+for key in hetero_graph.node_label:
+    hetero_graph.node_label[key] = hetero_graph.node_label[key].to(args['device'])
+
+# Edge_index to sparse tensor and to device
+for key in hetero_graph.edge_index:
+    edge_index = hetero_graph.edge_index[key]
+    adj = SparseTensor(row=edge_index[0], col=edge_index[1], sparse_sizes=(hetero_graph.num_nodes('author'), hetero_graph.num_nodes('author')))
+    hetero_graph.edge_index[key] = adj.t().to(args['device'])
+
+
+# 
+edge_index = data.edge_index
+data.edge_weight = data.edge_weight.view(-1).to(torch.float)
+data = T.ToSparseTensor()(data)
+
+split_edge = dataset.get_edge_split()
+
+model = HeteroGNN(hetero_graph, args, aggr="mean").to(args['device'])
+
+predictor = LinkPredictor(args['hidden_size'], args['hidden_size'], 1,
+                            3, dropout=0).to(args['device'])
+
+evaluator = Evaluator(name='ogbl-collab')
+loggers = {
+    'Hits@10': Logger(args['run'], args),
+    'Hits@50': Logger(args['run'], args),
+    'Hits@100': Logger(args['run'], args),
+}
+
+
+for run in range(args['run']):
+    model.reset_parameters()
+    predictor.reset_parameters()
+    optimizer = torch.optim.Adam(
+        list(model.parameters()) + list(predictor.parameters()),
+        lr=args['lr'])
+
+    for epoch in range(1, 1 + args['epochs']):
+        loss = train(model, predictor, hetero_graph, split_edge, optimizer, args['batch_size'])
+
+        if epoch % args['eval_steps'] == 0:
+            results = test(model, predictor, data, split_edge, evaluator,
+                            args.batch_size)
+            for key, result in results.items():
+                loggers[key].add_result(run, result)
+
+            if epoch % args['log_steps'] == 0:
+                for key, result in results.items():
+                    train_hits, valid_hits, test_hits = result
+                    print(key)
+                    print(f'Run: {run + 1:02d}, '
+                            f'Epoch: {epoch:02d}, '
+                            f'Loss: {loss:.4f}, '
+                            f'Train: {100 * train_hits:.2f}%, '
+                            f'Valid: {100 * valid_hits:.2f}%, '
+                            f'Test: {100 * test_hits:.2f}%')
+                print('---')
+
+    for key in loggers.keys():
+        print(key)
+        loggers[key].print_statistics(run)
+
+for key in loggers.keys():
+    print(key)
+    loggers[key].print_statistics()
---
+++ hete_models.py
@@ -0,0 +1,340 @@
+"""
+Heterougeous model import to hete_graph.py
+"""
+import copy
+import torch
+import deepsnap
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch_geometric.nn as pyg_nn
+from torch_scatter import scatter
+
+from sklearn.metrics import f1_score
+from deepsnap.hetero_gnn import forward_op
+from deepsnap.hetero_graph import HeteroGraph
+from torch_sparse import SparseTensor, matmul
+
+class HeteroGNNConv(pyg_nn.MessagePassing):
+    def __init__(self, in_channels_src, in_channels_dst, out_channels):
+        super(HeteroGNNConv, self).__init__(aggr="mean")
+
+        self.in_channels_src = in_channels_src
+        self.in_channels_dst = in_channels_dst
+        self.out_channels = out_channels
+
+        # To simplify implementation, please initialize both self.lin_dst
+        # and self.lin_src out_features to out_channels
+        self.lin_dst = None
+        self.lin_src = None
+
+        self.lin_update = None
+
+        ############# Your code here #############
+        ## (~3 lines of code)
+        self.lin_dst = nn.Linear(self.in_channels_dst, out_channels)
+        self.lin_src = nn.Linear(self.in_channels_src, out_channels)  
+        self.lin_update = nn.Linear(out_channels*2, out_channels) # 
+
+
+        ##########################################
+
+    def reset_parameters(self):
+        self.lin_dst.reset_parameters()
+        self.lin_src.reset_parameters()
+        self.lin_update.reset_parameters()
+
+    def forward(
+        self,
+        node_feature_src,
+        node_feature_dst,
+        edge_index,
+        size=None,
+        res_n_id=None,
+    ):
+        ############# Your code here #############
+        ## (~1 line of code)
+        out = self.propagate(edge_index, node_feature_src=node_feature_src, node_feature_dst=node_feature_dst, size=size, res_n_id=res_n_id)
+        return out
+        ##########################################
+
+    def message_and_aggregate(self, edge_index, node_feature_src):
+
+        ############# Your code here #############
+        ## (~1 line of code)
+        ## Note:
+        ## 1. Different from what we implemented in Colab 3, we use message_and_aggregate
+        ## to replace the message and aggregate. The benefit is that we can avoid
+        ## materializing x_i and x_j, and make the implementation more efficient.
+        ## 2. To implement efficiently, following PyG documentation is helpful:
+        ## https://pytorch-geometric.readthedocs.io/en/latest/notes/sparse_tensor.html
+        ## 3. Here edge_index is torch_sparse SparseTensor.
+        out = matmul(edge_index, node_feature_src, reduce='mean')
+        ##########################################
+
+        return out
+
+    def update(self, aggr_out, node_feature_dst, res_n_id):
+
+        ############# Your code here #############
+        ## (~4 lines of code)
+        # Takes in the output of aggregation as first argument
+        src = self.lin_src(aggr_out) 
+        dst = self.lin_dst(node_feature_dst)
+        concat = torch.cat((dst, src), dim=1) # (B, hidden_dim)
+        aggr_out = self.lin_update(concat)
+
+        ##########################################
+
+        return aggr_out
+
+
+class HeteroGNNWrapperConv(deepsnap.hetero_gnn.HeteroConv):
+    def __init__(self, convs, args, aggr="mean"):
+        super(HeteroGNNWrapperConv, self).__init__(convs, None)
+        self.aggr = aggr
+
+        # Map the index and message type
+        self.mapping = {}
+
+        # A numpy array that stores the final attention probability
+        self.alpha = None
+
+        self.attn_proj = None
+
+        if self.aggr == "attn":
+            ############# Your code here #############
+            ## (~1 line of code)
+            ## Note:
+            ## 1. Initialize self.attn_proj here.
+            ## 2. You should use nn.Sequential for self.attn_proj
+            ## 3. nn.Linear and nn.Tanh are useful.
+            ## 4. You can create a vector parameter by using:
+            ## nn.Linear(some_size, 1, bias=False)
+            ## 5. The first linear layer should have out_features as args['attn_size']
+            ## 6. You can assume we only have one "head" for the attention.
+            ## 7. We recommend you to implement the mean aggregation first. After 
+            ## the mean aggregation works well in the training, then you can 
+            ## implement this part.
+            self.aggr_proj = nn.Sequential(nn.Linear(in_features=args['hidden_size'], out_features=args['attn_size']),
+                                           nn.Tanh(),
+                                           nn.Linear(args['attn_size'], 1, bias=False))
+
+            ##########################################
+            
+    def reset_parameters(self):
+        super(HeteroGNNWrapperConv, self).reset_parameters()
+        if self.aggr == "attn":
+            for layer in self.attn_proj.children():
+                layer.reset_parameters()
+    
+    def forward(self, node_features, edge_indices):
+        message_type_emb = {}
+        for message_key, message_type in edge_indices.items():
+            src_type, edge_type, dst_type = message_key
+            node_feature_src = node_features[src_type]
+            node_feature_dst = node_features[dst_type]
+            edge_index = edge_indices[message_key]
+            message_type_emb[message_key] = (
+                self.convs[message_key](
+                    node_feature_src,
+                    node_feature_dst,
+                    edge_index,
+                )
+            )
+        node_emb = {dst: [] for _, _, dst in message_type_emb.keys()}
+        mapping = {}        
+        for (src, edge_type, dst), item in message_type_emb.items():
+            mapping[len(node_emb[dst])] = (src, edge_type, dst)
+            node_emb[dst].append(item)
+        self.mapping = mapping
+        for node_type, embs in node_emb.items():
+            if len(embs) == 1:
+                node_emb[node_type] = embs[0]
+            else:
+                node_emb[node_type] = self.aggregate(embs)
+        return node_emb
+    
+    def aggregate(self, xs):
+        # TODO: Implement this function that aggregates all message type results.
+        # Here, xs is a list of tensors (embeddings) with respect to message 
+        # type aggregation results.
+
+        if self.aggr == "mean":
+
+            ############# Your code here #############
+            ## (~2 lines of code)
+            out = torch.mean(torch.stack(xs), dim=0)
+            return out
+            ##########################################
+
+        elif self.aggr == "attn":
+
+            ############# Your code here #############
+            ## (~10 lines of code)
+            ## Note:
+            ## 1. Store the value of attention alpha (as a numpy array) to self.alpha,
+            ## which has the shape (len(xs), ) self.alpha will be not be used 
+            ## to backpropagate etc. in the model. We will use it to see how much 
+            ## attention the layer pays on different message types.
+            ## 2. torch.softmax and torch.cat are useful.
+            ## 3. You might need to reshape the tensors by using the 
+            ## `view()` function https://pytorch.org/docs/stable/tensor_view.html
+            
+            # Compute e_m for every message
+            ems = torch.zeros((len(xs),), device=args['device'])
+            for i in range(len(xs)):
+                m = xs[i]
+                em = self.aggr_proj(m) # (3025, 1) 3025 nodes
+                em = torch.mean(em) 
+                ems[i] = em
+            # Compute alpha
+            alpha = torch.softmax(ems, dim=0)
+            # alpha = alpha.cuda()
+            reshape_xs = torch.cat(xs, dim=1).view(xs[0].shape[0], len(xs), xs[0].shape[1]) # (Vd, M, num of message, hidden_size=64)
+            out = torch.matmul(alpha, reshape_xs) 
+            # print(alpha[0] * xs[0][1] + alpha[1] * xs[1][1])
+            # print(out[1]) 
+            # print(alpha[0] * xs[0][1] + alpha[1] * xs[1][1] - out[1])
+            # assert (alpha[0] * xs[0][1] + alpha[1] * xs[1][1] == out[1]).all()
+            self.alpha = alpha.cpu().detach().numpy()
+            return out
+
+            ##########################################
+
+
+def generate_convs(hetero_graph, conv, hidden_size, first_layer=False):
+    # TODO: Implement this function that returns a dictionary of `HeteroGNNConv` 
+    # layers where the keys are message types. `hetero_graph` is deepsnap `HeteroGraph`
+    # object and the `conv` is the `HeteroGNNConv`.
+
+    convs = {}
+
+    ############# Your code here #############
+    ## (~9 lines of code)
+    mtype = hetero_graph.message_types
+    for mt in mtype:
+        src, edge, dst = mt 
+        if first_layer:
+            src_dim = hetero_graph.num_node_features(src)
+            dst_dim = hetero_graph.num_node_features(dst)
+            convs[mt] = conv(in_channels_src=src_dim, in_channels_dst=dst_dim, out_channels=hidden_size)
+        else:
+            convs[mt] = conv(in_channels_src=hidden_size, in_channels_dst=hidden_size, out_channels=hidden_size)
+    ##########################################
+    
+    return convs
+
+
+
+class HeteroGNN(torch.nn.Module):
+    def __init__(self, hetero_graph, args, aggr="mean"):
+        super(HeteroGNN, self).__init__()
+        
+        self.graph = hetero_graph
+        self.aggr = aggr
+        self.hidden_size = args['hidden_size']
+
+        self.convs1 = None
+        self.convs2 = None
+
+        self.bns1 = nn.ModuleDict()
+        self.bns2 = nn.ModuleDict()
+        self.relus1 = nn.ModuleDict()
+        self.relus2 = nn.ModuleDict()
+        self.post_mps = nn.ModuleDict()
+
+        ############# Your code here #############
+        ## (~10 lines of code)
+        ## Note:
+        ## 1. For self.convs1 and self.convs2, call generate_convs at first and then
+        ## pass the returned dictionary of `HeteroGNNConv` to `HeteroGNNWrapperConv`.
+        ## 2. For self.bns, self.relus and self.post_mps, the keys are node_types.
+        ## `deepsnap.hetero_graph.HeteroGraph.node_types` will be helpful.
+        ## 3. Initialize all batchnorms to torch.nn.BatchNorm1d(hidden_size, eps=1.0).
+        ## 4. Initialize all relus to nn.LeakyReLU().
+        ## 5. For self.post_mps, each value in the ModuleDict is a linear layer 
+        ## where the `out_features` is the number of classes for that node type.
+        ## `deepsnap.hetero_graph.HeteroGraph.num_node_labels(node_type)` will be
+        ## useful.
+        self.convs1 = HeteroGNNWrapperConv(generate_convs(hetero_graph, HeteroGNNConv, self.hidden_size, first_layer=True), args, aggr=self.aggr)
+        self.convs2 = HeteroGNNWrapperConv(generate_convs(hetero_graph, HeteroGNNConv, self.hidden_size), args, aggr=self.aggr)
+        for nt in hetero_graph.node_types:
+            self.bns1[nt] = torch.nn.BatchNorm1d(self.hidden_size, eps=1.0)
+            self.bns2[nt] = torch.nn.BatchNorm1d(self.hidden_size, eps=1.0)
+            self.relus1[nt] = nn.LeakyReLU()
+            self.relus2[nt] = nn.LeakyReLU()
+            self.post_mps[nt] = nn.Linear(self.hidden_size, hetero_graph.num_node_labels(nt))
+
+        ##########################################
+
+    def reset_parameters(self):
+        self.convs1.reset_parameters()
+        self.convs2.reset_parameters()
+        for nt in self.graph.node_types:
+            self.bns1[nt].reset_parameters()
+            self.bns2[nt].reset_parameters()
+            self.post_mps[nt].reset_parameters()
+
+    def forward(self, node_feature, edge_index):
+        # TODO: Implement the forward function. Notice that `node_feature` is 
+        # a dictionary of tensors where keys are node types and values are 
+        # corresponding feature tensors. The `edge_index` is a dictionary of 
+        # tensors where keys are message types and values are corresponding
+        # edge index tensors (with respect to each message type).
+
+        x = node_feature
+
+        ############# Your code here #############
+        ## (~7 lines of code)
+        ## Note:
+        ## 1. `deepsnap.hetero_gnn.forward_op` can be helpful.
+        x = self.convs1(x, edge_index)
+        x = forward_op(x, self.bns1)
+        x = forward_op(x, self.relus1)
+        x = self.convs2(x, edge_index)
+        ##########################################
+        return x
+
+    def loss(self, preds, y, indices):
+        
+        loss = 0
+        loss_func = F.cross_entropy
+
+        ############# Your code here #############
+        ## (~3 lines of code)
+        ## Note:
+        ## 1. For each node type in preds, accumulate computed loss to `loss`
+        ## 2. Loss need to be computed with respect to the given index
+        for k in preds:
+            loss += loss_func(preds[k][indices[k]], y[k][indices[k]])
+        ##########################################
+
+        return loss
+
+
+class LinkPredictor(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers,
+                 dropout):
+        super(LinkPredictor, self).__init__()
+
+        self.lins = torch.nn.ModuleList()
+        self.lins.append(torch.nn.Linear(in_channels, hidden_channels))
+        for _ in range(num_layers - 2):
+            self.lins.append(torch.nn.Linear(hidden_channels, hidden_channels))
+        self.lins.append(torch.nn.Linear(hidden_channels, out_channels))
+
+        self.dropout = dropout
+
+    def reset_parameters(self):
+        for lin in self.lins:
+            lin.reset_parameters()
+
+    def forward(self, x_i, x_j):
+        x = x_i * x_j
+        for lin in self.lins[:-1]:
+            x = lin(x)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.lins[-1](x)
+        return torch.sigmoid(x)
