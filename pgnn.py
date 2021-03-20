a21c429a656817c52eb53f7660fffff5cb145f25
Zhejian Peng <ZHEJIANPENG@YAHOO.COM>
2021-03-20 00:28:10 -0500

    pgnn

---
+++ pgnn.py
@@ -0,0 +1,209 @@
+"""
+Add node to node distance in graphs
+"""
+
+import torch
+from torch_geometric.data import DataLoader
+import torch.optim as optim
+import os
+
+
+from tqdm import tqdm
+import argparse
+import time
+import numpy as np
+
+### importing OGB
+from ogb.graphproppred import PygGraphPropPredDataset, Evaluator
+from model import *
+from utils import *
+
+from tensorboardX import SummaryWriter
+
+log_dir = os.path.join('./log', "RUN_" + str(0))
+writer = SummaryWriter(log_dir=log_dir)
+
+cls_criterion = torch.nn.BCEWithLogitsLoss()
+reg_criterion = torch.nn.MSELoss()
+
+def train(model, device, loader, optimizer, task_type, args):
+    model.train()
+
+    train_loss = 0
+    for step, batch in enumerate(tqdm(loader, desc="Iteration")):
+        # if batch.x.shape[0] == 1 or batch.batch[-1] == 0:
+        #     pass
+        # else:
+        # Compute distance
+        graph_idx, batch = batch
+        # Find dists using graph_idx
+        dists = precompute_dist_data(batch.edge_index.numpy(), batch.num_nodes, approximate=-1)
+        batch.dists = torch.from_numpy(dists).float()
+        preselect_anchor(batch, layer_num=args.layer_num, anchor_num=args.anchor_num, device='cpu')
+
+        batch = batch.to(device)
+        pred = model(batch)
+        optimizer.zero_grad()
+        ## ignore nan targets (unlabeled) when computing training loss.
+        is_labeled = batch.y == batch.y
+        if "classification" in task_type: 
+            loss = cls_criterion(pred.to(torch.float32)[is_labeled], batch.y.to(torch.float32)[is_labeled])
+        else:
+            loss = reg_criterion(pred.to(torch.float32)[is_labeled], batch.y.to(torch.float32)[is_labeled])
+        train_loss += loss
+        loss.backward()
+        optimizer.step()
+    return train_loss
+  
+
+def eval(model, device, loader, evaluator, args):
+    model.eval()
+    y_true = []
+    y_pred = []
+
+    for step, batch in enumerate(tqdm(loader, desc="Iteration")):
+        dists = precompute_dist_data(batch.edge_index.numpy(), batch.num_nodes, approximate=-1)
+        batch.dists = torch.from_numpy(dists).float()
+        preselect_anchor(batch, layer_num=args.layer_num, anchor_num=args.anchor_num, device='cpu')
+        batch = batch.to(device)
+
+        if batch.x.shape[0] == 1:
+            pass
+        else:
+            with torch.no_grad():
+                pred = model(batch)
+
+            y_true.append(batch.y.view(pred.shape).detach().cpu())
+            y_pred.append(pred.detach().cpu())
+
+    y_true = torch.cat(y_true, dim = 0).numpy()
+    y_pred = torch.cat(y_pred, dim = 0).numpy()
+
+    input_dict = {"y_true": y_true, "y_pred": y_pred}
+
+    return evaluator.eval(input_dict)
+    
+parser = argparse.ArgumentParser()
+# GNN settings
+parser.add_argument('--device', type=int, default=0,
+                        help='which gpu to use if any (default: 0)')
+parser.add_argument('--use_val_as_input', action='store_true')
+parser.add_argument('--graph_pooling', dest='graph_pooling', default='mean', type=str)
+parser.add_argument('--lr', type=float, default=0.001)
+parser.add_argument('--epochs', dest='epochs', default=400, type=int) # implemented via accumulating gradient
+parser.add_argument('--eval_step', dest='eval_steps', default=1, type=int) # implemented via accumulating gradient
+parser.add_argument('--log_step', dest='log_step', default=1, type=int) # implemented via accumulating gradient
+parser.add_argument('--batch_size', dest='batch_size', default=1, type=int) # implemented via accumulating gradient
+parser.add_argument('--layer_num', dest='layer_num', default=2, type=int)
+parser.add_argument('--feature_dim', dest='feature_dim', default=32, type=int)
+parser.add_argument('--hidden_dim', dest='hidden_dim', default=32, type=int)
+parser.add_argument('--output_dim', dest='output_dim', default=32, type=int)
+parser.add_argument('--anchor_num', dest='anchor_num', default=4, type=int)
+parser.add_argument('--num_hops', dest='num_hops', default=1, type=int)
+parser.add_argument('--permute', dest='permute', default=True,
+                    help='whether permute subsets')
+parser.add_argument('--dropout', dest='dropout', action='store_true',
+                    help='whether dropout, default 0.5')
+parser.add_argument('--dropout_no', dest='dropout', action='store_false',
+                    help='whether dropout, default 0.5')
+parser.add_argument('--feature_pre', dest='feature_pre', action='store_true',
+                        help='whether pre transform feature')
+parser.add_argument('--filename', type=str, default="",
+                        help='filename to output result (default: )')
+parser.add_argument('--feature', type=str, default="full",
+                        help='full feature or simple feature')
+
+parser.add_argument('--runs', type=int, default=10)
+parser.add_argument('--num_workers', type=int, default=0)
+args = parser.parse_args(args=[])
+# args = parser.parse_args()
+
+### automatic dataloading and splitting
+dataset = PygGraphPropPredDataset(name ='ogbg-molhiv')
+
+device = torch.device("cuda:" + str(args.device)) if torch.cuda.is_available() else torch.device("cpu")
+
+precompute_distance = {}
+for i, (_, data) in enumerate(dataset):
+    dists = precompute_dist_data(data.edge_index.numpy(), data.num_nodes, approximate=-1)
+    # dists = torch.from_numpy(dists).float() 
+    precompute_distance[0] = dists 
+
+np.save('dists.npy', precompute_distance)
+precompute_distance = np.load('dists.npy', allow_pickle=True)
+
+
+# Save dataset object
+if args.feature == 'full':
+    pass 
+elif args.feature == 'simple':
+    print('using simple feature')
+    # only retain the top two node/edge features
+    dataset.data.x = dataset.data.x[:,:2]
+    dataset.data.edge_attr = dataset.data.edge_attr[:,:2]
+    
+
+split_idx = dataset.get_idx_split()
+evaluator = Evaluator(name ='ogbg-molhiv')
+
+# TODO: Precompute distance and map each graph index
+train_loader = DataLoader(dataset[split_idx["train"]], batch_size=args.batch_size, shuffle=True, num_workers = args.num_workers)
+valid_loader = DataLoader(dataset[split_idx["valid"]], batch_size=args.batch_size, shuffle=False, num_workers = args.num_workers)
+test_loader = DataLoader(dataset[split_idx["test"]], batch_size=args.batch_size, shuffle=False, num_workers = args.num_workers)
+
+num_features = 9 # dataset[0][1].x.shape[1] # use dataset.idx
+model = PGNN(input_dim=num_features, feature_dim=args.feature_dim,
+            hidden_dim=args.hidden_dim, output_dim=args.output_dim,
+            feature_pre=args.feature_pre, layer_num=args.layer_num, 
+            dropout=args.dropout, graph_pooling=args.graph_pooling).to(device)
+
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+
+valid_curve = []
+test_curve = []
+# train_curve = []
+
+print("Start")
+for epoch in range(1, args.epochs + 1):
+    print("=====Epoch {}".format(epoch))
+    print('Training...')
+    loss = train(model, device, train_loader, optimizer, dataset.task_type, args)
+
+    print('Evaluating...')
+    # train_perf = eval(model, device, train_loader, evaluator, args)
+    valid_perf = eval(model, device, valid_loader, evaluator, args)
+    test_perf = eval(model, device, test_loader, evaluator, args)
+
+    print({'Train': train_perf, 'Validation': valid_perf, 'Test': test_perf})
+
+    # train_curve.append(train_perf[dataset.eval_metric])
+    valid_curve.append(valid_perf[dataset.eval_metric])
+    test_curve.append(test_perf[dataset.eval_metric])
+
+
+
+    if 'classification' in dataset.task_type:
+        best_val_epoch = np.argmax(np.array(valid_curve))
+        # best_train = max(train_curve)
+    else:
+        best_val_epoch = np.argmin(np.array(valid_curve))
+        # best_train = min(train_curve)
+
+    writer.add_scalar('train/_loss', loss, epoch)
+    
+    writer.add_scalar('val/_perf', valid_perf, epoch)
+    
+    writer.add_scalar('test/_perf', test_curve, epoch)
+    
+    writer.add_scalar('learning_rate', optimizer.param_groups[0]['lr'], epoch) 
+
+
+    print('Finished training!')
+    print('Best validation score: {}'.format(valid_curve[best_val_epoch]))
+    print('Test score: {}'.format(test_curve[best_val_epoch]))
+
+if not args.filename == '':
+    torch.save({'Val': valid_curve[best_val_epoch], 'Test': test_curve[best_val_epoch], 'Train': [best_val_epoch]}, args.filename)
+
+
---
+++ utils.py
@@ -0,0 +1,431 @@
+import torch
+import networkx as nx
+import numpy as np
+import multiprocessing as mp
+import random
+from torch.utils.data import Dataset
+
+
+
+# # approximate
+def get_edge_mask_link_negative_approximate(mask_link_positive, num_nodes, num_negtive_edges):
+    links_temp = np.zeros((num_nodes, num_nodes)) + np.identity(num_nodes)
+    mask_link_positive = duplicate_edges(mask_link_positive)
+    links_temp[mask_link_positive[0],mask_link_positive[1]] = 1
+    # add random noise
+    links_temp += np.random.rand(num_nodes,num_nodes)
+    prob = num_negtive_edges / (num_nodes*num_nodes-mask_link_positive.shape[1])
+    mask_link_negative = np.stack(np.nonzero(links_temp<prob))
+    return mask_link_negative
+
+
+# exact version, slower
+def get_edge_mask_link_negative(mask_link_positive, num_nodes, num_negtive_edges):
+    mask_link_positive_set = []
+    for i in range(mask_link_positive.shape[1]):
+        mask_link_positive_set.append(tuple(mask_link_positive[:,i]))
+    mask_link_positive_set = set(mask_link_positive_set)
+
+    mask_link_negative = np.zeros((2,num_negtive_edges), dtype=mask_link_positive.dtype)
+    for i in range(num_negtive_edges):
+        while True:
+            mask_temp = tuple(np.random.choice(num_nodes,size=(2,),replace=False))
+            if mask_temp not in mask_link_positive_set:
+                mask_link_negative[:,i] = mask_temp
+                break
+
+    return mask_link_negative
+
+def resample_edge_mask_link_negative(data):
+    data.mask_link_negative_train = get_edge_mask_link_negative(data.mask_link_positive_train, num_nodes=data.num_nodes,
+                                                      num_negtive_edges=data.mask_link_positive_train.shape[1])
+    data.mask_link_negative_val = get_edge_mask_link_negative(data.mask_link_positive, num_nodes=data.num_nodes,
+                                                      num_negtive_edges=data.mask_link_positive_val.shape[1])
+    data.mask_link_negative_test = get_edge_mask_link_negative(data.mask_link_positive, num_nodes=data.num_nodes,
+                                                     num_negtive_edges=data.mask_link_positive_test.shape[1])
+
+
+def deduplicate_edges(edges):
+    edges_new = np.zeros((2,edges.shape[1]//2), dtype=int)
+    # add none self edge
+    j = 0
+    skip_node = {} # node already put into result
+    for i in range(edges.shape[1]):
+        if edges[0,i]<edges[1,i]:
+            edges_new[:,j] = edges[:,i]
+            j += 1
+        elif edges[0,i]==edges[1,i] and edges[0,i] not in skip_node:
+            edges_new[:,j] = edges[:,i]
+            skip_node.add(edges[0,i])
+            j += 1
+
+    return edges_new
+
+def duplicate_edges(edges):
+    return np.concatenate((edges, edges[::-1,:]), axis=-1)
+
+
+# each node at least remain in the new graph
+def split_edges(edges, remove_ratio, connected=False):
+    e = edges.shape[1]
+    edges = edges[:, np.random.permutation(e)]
+    if connected:
+        unique, counts = np.unique(edges, return_counts=True)
+        node_count = dict(zip(unique, counts))
+
+        index_train = []
+        index_val = []
+        for i in range(e):
+            node1 = edges[0,i]
+            node2 = edges[1,i]
+            if node_count[node1]>1 and node_count[node2]>1: # if degree>1
+                index_val.append(i)
+                node_count[node1] -= 1
+                node_count[node2] -= 1
+                if len(index_val) == int(e * remove_ratio):
+                    break
+            else:
+                index_train.append(i)
+        index_train = index_train + list(range(i + 1, e))
+        index_test = index_val[:len(index_val)//2]
+        index_val = index_val[len(index_val)//2:]
+
+        edges_train = edges[:, index_train]
+        edges_val = edges[:, index_val]
+        edges_test = edges[:, index_test]
+    else:
+        split1 = int((1-remove_ratio)*e)
+        split2 = int((1-remove_ratio/2)*e)
+        edges_train = edges[:,:split1]
+        edges_val = edges[:,split1:split2]
+        edges_test = edges[:,split2:]
+
+    return edges_train, edges_val, edges_test
+
+
+
+
+def edge_to_set(edges):
+    edge_set = []
+    for i in range(edges.shape[1]):
+        edge_set.append(tuple(edges[:, i]))
+    edge_set = set(edge_set)
+    return edge_set
+
+
+def get_link_mask(data, remove_ratio=0.2, resplit=True, infer_link_positive=True):
+    if resplit:
+        if infer_link_positive:
+            data.mask_link_positive = deduplicate_edges(data.edge_index.numpy())
+        data.mask_link_positive_train, data.mask_link_positive_val, data.mask_link_positive_test = \
+            split_edges(data.mask_link_positive, remove_ratio)
+    resample_edge_mask_link_negative(data)
+
+
+def add_nx_graph(data):
+    G = nx.Graph()
+    edge_numpy = data.edge_index.numpy()
+    edge_list = []
+    for i in range(data.num_edges):
+        edge_list.append(tuple(edge_numpy[:, i]))
+    G.add_edges_from(edge_list)
+    data.G = G
+
+def single_source_shortest_path_length_range(graph, node_range, cutoff):
+    dists_dict = {}
+    for node in node_range:
+        dists_dict[node] = nx.single_source_shortest_path_length(graph, node, cutoff)
+    return dists_dict
+
+def merge_dicts(dicts):
+    result = {}
+    for dictionary in dicts:
+        result.update(dictionary)
+    return result
+
+def all_pairs_shortest_path_length_parallel(graph,cutoff=None,num_workers=4):
+    nodes = list(graph.nodes)
+    random.shuffle(nodes)
+    if len(nodes)<50:
+        num_workers = int(num_workers/4)
+    elif len(nodes)<400:
+        num_workers = int(num_workers/2)
+
+    pool = mp.Pool(processes=num_workers)
+    results = [pool.apply_async(single_source_shortest_path_length_range,
+            args=(graph, nodes[int(len(nodes)/num_workers*i):int(len(nodes)/num_workers*(i+1))], cutoff)) for i in range(num_workers)]
+    output = [p.get() for p in results]
+    dists_dict = merge_dicts(output)
+    pool.close()
+    pool.join()
+    return dists_dict
+
+
+def precompute_dist_data(edge_index, num_nodes, approximate=0):
+    '''
+    Here dist is 1/real_dist, higher actually means closer, 0 means disconnected
+    :return:
+    '''
+    graph = nx.Graph()
+    edge_list = edge_index.transpose(1,0).tolist()
+    graph.add_edges_from(edge_list)
+
+    n = num_nodes
+    dists_array = np.zeros((n, n))
+    # dists_dict = nx.all_pairs_shortest_path_length(graph,cutoff=approximate if approximate>0 else None)
+    # dists_dict = {c[0]: c[1] for c in dists_dict}
+    dists_dict = all_pairs_shortest_path_length_parallel(graph,cutoff=approximate if approximate>0 else None)
+    for i, node_i in enumerate(graph.nodes()):
+        shortest_dist = dists_dict[node_i]
+        for j, node_j in enumerate(graph.nodes()):
+            dist = shortest_dist.get(node_j, -1)
+            if dist!=-1:
+                # dists_array[i, j] = 1 / (dist + 1)
+                dists_array[node_i, node_j] = 1 / (dist + 1)
+    return dists_array
+
+
+
+def get_random_anchorset(n,c=0.5):
+    m = int(np.log2(n))
+    copy = int(c*m)
+    anchorset_id = []
+    for i in range(m):
+        anchor_size = int(n/np.exp2(i + 1))
+        for j in range(copy):
+            anchorset_id.append(np.random.choice(n,size=anchor_size,replace=False))
+    return anchorset_id
+
+def get_dist_max(anchorset_id, dist, device):
+    dist_max = torch.zeros((dist.shape[0],len(anchorset_id))).to(device)
+    dist_argmax = torch.zeros((dist.shape[0],len(anchorset_id))).long().to(device)
+    for i in range(len(anchorset_id)):
+        temp_id = torch.as_tensor(anchorset_id[i], dtype=torch.long)
+        dist_temp = dist[:, temp_id]
+        dist_max_temp, dist_argmax_temp = torch.max(dist_temp, dim=-1)
+        dist_max[:,i] = dist_max_temp
+        dist_argmax[:,i] = temp_id[dist_argmax_temp]
+    return dist_max, dist_argmax
+
+
+def preselect_anchor(data, layer_num=1, anchor_num=32, anchor_size_num=4, device='cpu'):
+
+    data.anchor_size_num = anchor_size_num
+    data.anchor_set = []
+    anchor_num_per_size = anchor_num//anchor_size_num
+    for i in range(anchor_size_num):
+        anchor_size = 2**(i+1)-1
+        anchors = np.random.choice(data.num_nodes, size=(layer_num,anchor_num_per_size,anchor_size), replace=True)
+        data.anchor_set.append(anchors)
+    data.anchor_set_indicator = np.zeros((layer_num, anchor_num, data.num_nodes), dtype=int)
+
+    anchorset_id = get_random_anchorset(data.num_nodes,c=1)
+    data.dists_max, data.dists_argmax = get_dist_max(anchorset_id, data.dists, device)
+
+
+
+class Dataset(torch.utils.data.Dataset):
+    r"""Dataset base class for creating graph datasets.
+    See `here <https://pytorch-geometric.readthedocs.io/en/latest/notes/
+    create_dataset.html>`__ for the accompanying tutorial.
+
+    Args:
+        root (string, optional): Root directory where the dataset should be
+            saved. (optional: :obj:`None`)
+        transform (callable, optional): A function/transform that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a transformed
+            version. The data object will be transformed before every access.
+            (default: :obj:`None`)
+        pre_transform (callable, optional): A function/transform that takes in
+            an :obj:`torch_geometric.data.Data` object and returns a
+            transformed version. The data object will be transformed before
+            being saved to disk. (default: :obj:`None`)
+        pre_filter (callable, optional): A function that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a boolean
+            value, indicating whether the data object should be included in the
+            final dataset. (default: :obj:`None`)
+    """
+    @property
+    def raw_file_names(self):
+        r"""The name of the files to find in the :obj:`self.raw_dir` folder in
+        order to skip the download."""
+        raise NotImplementedError
+
+    @property
+    def processed_file_names(self):
+        r"""The name of the files to find in the :obj:`self.processed_dir`
+        folder in order to skip the processing."""
+        raise NotImplementedError
+
+    def download(self):
+        r"""Downloads the dataset to the :obj:`self.raw_dir` folder."""
+        raise NotImplementedError
+
+    def process(self):
+        r"""Processes the dataset to the :obj:`self.processed_dir` folder."""
+        raise NotImplementedError
+
+    def len(self):
+        raise NotImplementedError
+
+    def get(self, idx):
+        r"""Gets the data object at index :obj:`idx`."""
+        raise NotImplementedError
+
+    def __init__(self, root=None, transform=None, pre_transform=None,
+                 pre_filter=None):
+        super(Dataset, self).__init__()
+
+        if isinstance(root, str):
+            root = osp.expanduser(osp.normpath(root))
+
+        self.root = root
+        self.transform = transform
+        self.pre_transform = pre_transform
+        self.pre_filter = pre_filter
+        self.__indices__ = None
+
+        if 'download' in self.__class__.__dict__.keys():
+            self._download()
+
+        if 'process' in self.__class__.__dict__.keys():
+            self._process()
+
+    def indices(self):
+        if self.__indices__ is not None:
+            return self.__indices__
+        else:
+            return range(len(self))
+
+    @property
+    def raw_dir(self):
+        return osp.join(self.root, 'raw')
+
+    @property
+    def processed_dir(self):
+        return osp.join(self.root, 'processed')
+
+    @property
+    def num_node_features(self):
+        r"""Returns the number of features per node in the dataset."""
+        return self[0].num_node_features
+
+    @property
+    def num_features(self):
+        r"""Alias for :py:attr:`~num_node_features`."""
+        return self.num_node_features
+
+    @property
+    def num_edge_features(self):
+        r"""Returns the number of features per edge in the dataset."""
+        return self[0].num_edge_features
+
+    @property
+    def raw_paths(self):
+        r"""The filepaths to find in order to skip the download."""
+        files = to_list(self.raw_file_names)
+        return [osp.join(self.raw_dir, f) for f in files]
+
+    @property
+    def processed_paths(self):
+        r"""The filepaths to find in the :obj:`self.processed_dir`
+        folder in order to skip the processing."""
+        files = to_list(self.processed_file_names)
+        return [osp.join(self.processed_dir, f) for f in files]
+
+    def _download(self):
+        if files_exist(self.raw_paths):  # pragma: no cover
+            return
+
+        makedirs(self.raw_dir)
+        self.download()
+
+    def _process(self):
+        f = osp.join(self.processed_dir, 'pre_transform.pt')
+        if osp.exists(f) and torch.load(f) != __repr__(self.pre_transform):
+            logging.warning(
+                'The `pre_transform` argument differs from the one used in '
+                'the pre-processed version of this dataset. If you really '
+                'want to make use of another pre-processing technique, make '
+                'sure to delete `{}` first.'.format(self.processed_dir))
+        f = osp.join(self.processed_dir, 'pre_filter.pt')
+        if osp.exists(f) and torch.load(f) != __repr__(self.pre_filter):
+            logging.warning(
+                'The `pre_filter` argument differs from the one used in the '
+                'pre-processed version of this dataset. If you really want to '
+                'make use of another pre-fitering technique, make sure to '
+                'delete `{}` first.'.format(self.processed_dir))
+
+        if files_exist(self.processed_paths):  # pragma: no cover
+            return
+
+        print('Processing...')
+
+        makedirs(self.processed_dir)
+        self.process()
+
+        path = osp.join(self.processed_dir, 'pre_transform.pt')
+        torch.save(__repr__(self.pre_transform), path)
+        path = osp.join(self.processed_dir, 'pre_filter.pt')
+        torch.save(__repr__(self.pre_filter), path)
+
+        print('Done!')
+
+    def __len__(self):
+        r"""The number of examples in the dataset."""
+        if self.__indices__ is not None:
+            return len(self.__indices__)
+        return self.len()
+
+    def __getitem__(self, idx):
+        r"""Gets the data object at index :obj:`idx` and transforms it (in case
+        a :obj:`self.transform` is given).
+        In case :obj:`idx` is a slicing object, *e.g.*, :obj:`[2:5]`, a list, a
+        tuple, a  LongTensor or a BoolTensor, will return a subset of the
+        dataset at the specified indices."""
+        if isinstance(idx, int):
+            data = self.get(self.indices()[idx])
+            data = data if self.transform is None else self.transform(data)
+            return data
+        else:
+            return self.index_select(idx)
+
+    def index_select(self, idx):
+        indices = self.indices()
+
+        if isinstance(idx, slice):
+            indices = indices[idx]
+        elif torch.is_tensor(idx):
+            if idx.dtype == torch.long:
+                if len(idx.shape) == 0:
+                    idx = idx.unsqueeze(0)
+                return self.index_select(idx.tolist())
+            elif idx.dtype == torch.bool or idx.dtype == torch.uint8:
+                return self.index_select(
+                    idx.nonzero(as_tuple=False).flatten().tolist())
+        elif isinstance(idx, list) or isinstance(idx, tuple):
+            indices = [indices[i] for i in idx]
+        else:
+            raise IndexError(
+                'Only integers, slices (`:`), list, tuples, and long or bool '
+                'tensors are valid indices (got {}).'.format(
+                    type(idx).__name__))
+
+        dataset = copy.copy(self)
+        dataset.__indices__ = indices
+        return dataset
+
+    def shuffle(self, return_perm=False):
+        r"""Randomly shuffles the examples in the dataset.
+
+        Args:
+            return_perm (bool, optional): If set to :obj:`True`, will
+                additionally return the random permutation used to shuffle the
+                dataset. (default: :obj:`False`)
+        """
+        perm = torch.randperm(len(self))
+        dataset = self.index_select(perm)
+        return (dataset, perm) if return_perm is True else dataset
+
+    def __repr__(self):  # pragma: no cover
+        return f'{self.__class__.__name__}({len(self)})'
